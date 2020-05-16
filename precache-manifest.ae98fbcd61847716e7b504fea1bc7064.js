f311adfe0b053042ce428cdddf9df8f144807cc3
jjohnsundar <jjohnsundar9943@gmail.com>
2020-05-16 16:28:38 +0530

    Updates

---
+++ precache-manifest.ae98fbcd61847716e7b504fea1bc7064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107acc2c6710a3de1780f9093ce4aaa1",
+    "url": "/Simplecalculator/index.html"
+  },
+  {
+    "revision": "22b62ac56bcd4308e876",
+    "url": "/Simplecalculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "625d9421f551e2ccb35c",
+    "url": "/Simplecalculator/static/js/2.4906145d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Simplecalculator/static/js/2.4906145d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b62ac56bcd4308e876",
+    "url": "/Simplecalculator/static/js/main.65fd19dd.chunk.js"
+  },
+  {
+    "revision": "9b042c265fde53d63491",
+    "url": "/Simplecalculator/static/js/runtime-main.dd9cdee1.js"
+  }
+]);
